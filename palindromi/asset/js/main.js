0293800b24f70dd14bb6b60f16422295e836c9e2
StefanoNesi <sn.boolean@gmail.com>
2021-10-19 13:46:13 +0200

    PALINDROME - get user string

---
+++ palindromi/asset/js/main.js
@@ -0,0 +1,18 @@
+//Palidroma 
+/*
+Ask the user for a word/sentence, then create a function to discover if the word/sentence is a palindrome
+
+*/
+
+const userString = prompt("Type a word or a sentence, i will tell you if it's a palindrome or not!");
+
+checkPalindrome(userString);
+
+function checkPalindrome(stringToCheck) {
+
+
+}
+
+
+
+
