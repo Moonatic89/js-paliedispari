47b149cdf90eb9baad55219e88d5efe4d0ffd6dc
StefanoNesi <sn.boolean@gmail.com>
2021-10-19 14:06:25 +0200

    PALINDROME - Logic done

--- palindromi/asset/js/main.js
+++ palindromi/asset/js/main.js
@@ -4,11 +4,30 @@ Ask the user for a word/sentence, then create a function to discover if the word
 
 */
 
-const userString = prompt("Type a word or a sentence, i will tell you if it's a palindrome or not!");
+//this is LET because i'll remove all spaces
+let userString = prompt("Type a word or a sentence, i will tell you if it's a palindrome or not!");
 
 checkPalindrome(userString);
 
 function checkPalindrome(stringToCheck) {
+
+    stringToCheck = stringToCheck.replace(/\s+/g, '');
+
+    const stringCopy = stringToCheck;
+
+    let i = 0;
+    let j = stringToCheck.length;
+
+    while (j > 0) {
+        if (stringToCheck[(j - 1)] == stringCopy[i]) {
+            console.log(stringToCheck[(j - 1)], stringCopy[i]);
+        }
+        i++;
+        j--;
+    }
+
+
+
 
 
 }
