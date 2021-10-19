b2f4d74d50d8ae21154660bb2aff4f9e77eb681b
StefanoNesi <sn.boolean@gmail.com>
2021-10-19 15:13:11 +0200

    Finished, starting bonus

--- pari_dispari/asset/js/main.js
+++ pari_dispari/asset/js/main.js
@@ -5,19 +5,26 @@ and outputs if the sum is odd or even. Then declares if the user won or not.
 
 */
 
-const userChoice = prompt("Please choose Even or Odd");
+const userChoice = prompt("Please choose Even or Odd").toLowerCase();
 const userNumber = parseInt(prompt("Please choose a number between 1 and 5, i will choose another number and sum it to yours. if you guessed the type, you win."));
+const element = document.querySelector("h1");
 let sumResult;
+let message;
 
 const cpuNumber = oneToFiveRNG();
 
 sumResult = sumAndDefineOddEven(userNumber, cpuNumber);
 
-declareWinner();
+declareWinner(sumResult);
 
 
 
-
+/**
+ * Generates a random number between 1 to five
+ * //
+ * //
+ * //
+ */
 function oneToFiveRNG() {
     return Math.floor(Math.random() * 5) + 1;
 }
@@ -34,4 +41,11 @@ function sumAndDefineOddEven(num1, num2) {
 
 }
 
-function declareWinner() { }
+function declareWinner(result) {
+    if (result == userChoice) {
+        message = `You win: your number was ${userNumber}, mine was ${cpuNumber}, their sum was ${userNumber + cpuNumber} and you choose ${userChoice}`
+    } else {
+        message = `You lost (or you choose a wrong type, so you lost anyway): your number was ${userNumber}, mine was ${cpuNumber}, their sum was ${userNumber + cpuNumber} and you choose ${userChoice}`
+    }
+    element.innerHTML = message;
+}
